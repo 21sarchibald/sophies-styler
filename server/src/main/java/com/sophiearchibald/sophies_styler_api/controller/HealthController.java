f85aae1c3c2976d4a115f84abf45114c6b2db9e7
Sophie Archibald <arc21004@byui.edu>
2026-07-11 20:52:58 -0600

    fixed endpoint path

--- server/src/main/java/com/sophiearchibald/sophies_styler_api/controller/HealthController.java
+++ server/src/main/java/com/sophiearchibald/sophies_styler_api/controller/HealthController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/api/health")
     public String health() {
         return "OK";
     }
